c6dffde8fd8b0762bc28be4df8470034a32e74b7
jeromevvb <jeromevvb@gmail.com>
2020-08-29 09:28:29 +0700

    init

--- App.js
+++ App.js
@@ -1,21 +1,60 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from "expo-status-bar";
+import React from "react";
+import {
+  StyleSheet,
+  Text,
+  View,
+  SafeAreaView,
+  Image,
+  Button,
+  Dimensions,
+  Alert,
+} from "react-native";
+import {
+  useDeviceOrientation,
+  useDimensions,
+} from "@react-native-community/hooks";
+
+import Login from "./views/Login";
 
 export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+  return <Login></Login>;
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+// const styles = StyleSheet.create({
+//   view: {
+//     backgroundColor: "blue",
+//     flex: 1,
+//   },
+// });
+// return (
+//   <View
+//     style={{
+//       backgroundColor: "white",
+//       flex: 1,
+//       flexDirection: "row-reverse",
+//       justifyContent: "center",
+//       alignContent: "center",
+//       // flexWrap: "wrap",
+//       // height: 80
+//     }}
+//   >
+//     <View
+//       style={{
+//         backgroundColor: "tomato",
+//         // width: 100,
+//         flex: 1,
+//         height: 100,
+//       }}
+//     />
+//     <View
+//       style={{
+//         backgroundColor: "dodgerblue",
+//         top: 20,
+//         width: 100,
+//         height: 100,
+//       }}
+//     />
+//     <View style={{ backgroundColor: "gold", width: 100, height: 100 }} />
+//   </View>
+// );
