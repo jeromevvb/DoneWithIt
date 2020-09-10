6d565eb59d9f0fc026d2fb13eeb2b55984eeca1c
jeromevvb <jeromevvb@gmail.com>
2020-09-10 17:04:37 +0700

    make list item touchable and swipeable

--- app/components/ListItem.js
+++ app/components/ListItem.js
@@ -1,19 +1,44 @@
 import React from "react";
-import { View, StyleSheet, Image } from "react-native";
+import { View, StyleSheet, Image, TouchableHighlight } from "react-native";
 import AppText from "./AppText";
 import colors from "../config/colors";
+import Swipeable from "react-native-gesture-handler/Swipeable";
+
+const RenderTouchable = ({ onPress, children }) => {
+  if (onPress) {
+    return (
+      <TouchableHighlight onPress={onPress} underlayColor={colors.light}>
+        {children}
+      </TouchableHighlight>
+    );
+  }
+
+  return children;
+};
+
+const RenderSwipeable = ({ rightActions, children }) => {
+  if (rightActions) {
+    return <Swipeable renderRightActions={rightActions}>{children}</Swipeable>;
+  }
+
+  return children;
+};
 
 const ListItem = (props) => {
-  const { avatar, title, subtitle } = props;
+  const { avatar, title, subtitle, onPress, rightActions } = props;
 
   return (
-    <View style={styles.container}>
-      <Image style={styles.avatar} source={avatar} />
-      <View style={styles.contentContainer}>
-        <AppText>Title</AppText>
-        <AppText style={styles.subtitle}>Subtitle</AppText>
-      </View>
-    </View>
+    <RenderSwipeable rightActions={rightActions}>
+      <RenderTouchable onPress={onPress}>
+        <View style={styles.container}>
+          <Image style={styles.avatar} source={avatar} />
+          <View style={styles.contentContainer}>
+            <AppText>{title}</AppText>
+            <AppText style={styles.subtitle}>{subtitle}</AppText>
+          </View>
+        </View>
+      </RenderTouchable>
+    </RenderSwipeable>
   );
 };
 
@@ -22,6 +47,7 @@ ListItem.defaultProps = {};
 const styles = StyleSheet.create({
   container: {
     flexDirection: "row",
+    padding: 15,
   },
   avatar: {
     width: 70,
