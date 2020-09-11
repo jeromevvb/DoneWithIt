0a0a9a9981bc9dcdbd09454b032ab631a2f595d3
jeromevvb <jeromevvb@gmail.com>
2020-09-11 15:53:00 +0700

    custom background color and add padding

--- app/components/SafeView.js
+++ app/components/SafeView.js
@@ -1,9 +1,27 @@
 import React from "react";
-import { StyleSheet, Platform, SafeAreaView, StatusBar } from "react-native";
+import {
+  StyleSheet,
+  Platform,
+  SafeAreaView,
+  StatusBar,
+  View,
+} from "react-native";
 import Constants from "expo-constants";
+import colors from "../config/colors";
 
-const SafeView = ({ children }) => {
-  return <SafeAreaView style={styles.container}>{children}</SafeAreaView>;
+const SafeView = ({ children, bgColor, padding = false }) => {
+  const innerStyles = [styles.container, { backgroundColor: bgColor }];
+  const paddingStyles = padding ? { padding: 20 } : {};
+
+  return (
+    <SafeAreaView style={innerStyles}>
+      <View style={paddingStyles}>{children}</View>
+    </SafeAreaView>
+  );
+};
+
+SafeView.defaultProps = {
+  bgColor: colors.white,
 };
 
 const styles = StyleSheet.create({
