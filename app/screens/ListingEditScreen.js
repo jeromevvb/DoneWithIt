57547c30f82464c8b55c2106652f80b84ba39706
jeromevvb <jeromevvb@gmail.com>
2020-09-16 12:43:20 +0700

    Implement FormImagePicker and ask for location

--- app/screens/ListingEditScreen.js
+++ app/screens/ListingEditScreen.js
@@ -1,12 +1,17 @@
-import React from "react";
+import React, { useEffect } from "react";
 import { View, StyleSheet } from "react-native";
 import SafeView from "../components/SafeView";
 import * as Yup from "yup";
 import { Form, FormField, SubmitButton, FormPicker } from "../components/forms";
 import { PickerIconItem } from "../components/picker";
+import FormImagePicker from "../components/forms/FormImagePicker";
+
+import * as Location from "expo-location";
+import * as Permissions from "expo-permissions";
 
 const validationSchema = Yup.object().shape({
   title: Yup.string().required().min(1).label("Title"),
+  pictures: Yup.array().min(1, "Please select at least one image").ensure(),
   price: Yup.number().required().min(1).max(10000).label("Price"),
   category: Yup.object().required().label("Category").nullable(),
   description: Yup.string().min(10).label("Description"),
@@ -57,6 +62,21 @@ const categories = [
 ];
 
 const ListingEditScreen = (props) => {
+  useEffect(() => {
+    const requestPermission = async () => {
+      const { granted } = Permissions.askAsync(Permissions.LOCATION);
+
+      if (!granted) return;
+
+      // for perfomance reasons, its better to get the last know position
+      const {
+        coords: { latitude, longitude },
+      } = Location.getLastKnownPositionAsync();
+    };
+
+    requestPermission();
+  }, []);
+
   return (
     <SafeView padding>
       <Form
@@ -64,6 +84,8 @@ const ListingEditScreen = (props) => {
         initialValues={validationSchema.default()}
         onSubmit={(values) => console.log(values)}
       >
+        <FormImagePicker name="pictures" />
+
         <FormField maxLength={255} name="title" placeholder="Title" />
 
         <FormField
@@ -89,7 +111,7 @@ const ListingEditScreen = (props) => {
           textAlignVertical="top"
         />
 
-        <SubmitButton title="Create" />
+        <SubmitButton title="POST" />
       </Form>
     </SafeView>
   );
