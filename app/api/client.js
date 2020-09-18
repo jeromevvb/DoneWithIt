a144bd16b9ebc095a023151a069eec1b27b5eb1c
jeromevvb <jeromevvb@gmail.com>
2020-09-18 16:01:30 +0700

    extend get request with cache

--- app/api/client.js
+++ app/api/client.js
@@ -1,7 +1,33 @@
 import { create } from "apisauce";
+import cache from "../utils/cache";
 
 const client = create({
   baseURL: "http://192.168.1.2:9000/api",
 });
+
+const get = client.get;
+
+client.get = async (url, params, axiosConfig) => {
+  const response = await get(url, params, axiosConfig);
+
+  // if success
+  if (response.ok) {
+    // cache data
+    cache.store(url, response.data);
+    // return object
+    return response;
+  }
+
+  // if the request got an error
+  // we checked if we have cached data
+  const cachedData = await cache.get(url);
+
+  // if we have data, we simulate a success request
+  if (cachedData) {
+    return { ok: true, data: cachedData };
+  }
+
+  return response;
+};
 
 export default client;
